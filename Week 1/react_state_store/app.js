6a156b2b4a3a430bb94bb292014427c37edf5514
Rixio Barrios <rbarrios@perscholas.org>
2022-06-07 14:01:42 -0400

    Update lesson

--- Week 1/react_state_store/app.js
+++ Week 1/react_state_store/app.js
@@ -30,27 +30,47 @@ constructor (props) {
 
     //new syntax
     state = {
-        data:data,
-        value:""
-    }
+        data: data,
+        name: '',
+        price: 0,
+        description: 'Describe this item'
+      }
 
-    handleChange(event){
+    handleChange = (event) =>{
         console.log(event.target.value)
-        this.setState({value: event.target.value})
-      }    
+        this.setState({[event.target.id]: event.target.value})
+      } 
 
     // handleChange = (event) =>{
     //     console.log(event.target.value)
     //     this.state.value = event.target.value
     //   }
+    handleSubmit = (event) =>{
+        event.preventDefault()
+        console.log('you prevented the default');
+      }
 
     render () {
         return (
             <div>
                 <h1>Big Time Shopping</h1>
-                <form>
-                <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='name'>Name</label>
+                    <input type='text' value={this.state.name} onChange={this.handleChange} id='name' placeholder='name of product' />
+                    <br />
+                    <label htmlFor='price'>Price</label>
+                    <input type='number' value={this.state.price} onChange={this.handleChange} id='price' />
+                    <br />
+                    <label htmlFor='description'>Description</label>
+                    <input type='textarea' value={this.state.description} onChange={this.handleChange} id='description' />
+                    <input type="submit" />
                 </form>
+                <div>
+                    <h2>Preview our new item</h2>
+                    <h3>{this.state.name}</h3>
+                    <h4>{this.state.price}</h4>
+                    <h5>{this.state.description}</h5>
+                </div>
                 <ul>
                 {this.state.data.map(data => {
                     return (
@@ -62,7 +82,6 @@ constructor (props) {
     )
     }
 }
-
 
 ReactDOM.render(
     <App />,
