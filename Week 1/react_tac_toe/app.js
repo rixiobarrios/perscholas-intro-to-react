731c34d8667dd2c5b6b21439c54b625a824bc1db
Rixio Barrios <rbarrios@perscholas.org>
2022-06-07 11:08:35 -0400

    Update lesson

--- Week 1/react_tac_toe/app.js
+++ Week 1/react_tac_toe/app.js
@@ -1,13 +1,35 @@
-class App extends React.Component {
+class Header extends React.Component {
     render() {
         return (
             <h1>React Tac Toe</h1>
         )
     }
 }
+class Player extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Player</h2>
+                <h3>Wins: </h3>
+            </div>
+        )
+    }
+}
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Player />
+                <Player />
+            </div>
+        )
+    }
+}
+
 
 
 ReactDOM.render(
-    <h1>React Tac Toe</h1>,
+    <App />,
     document.querySelector(".container")
 )
