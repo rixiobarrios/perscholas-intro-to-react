4f9f10a3d36358866a1aa552de6a871d000d3d80
Rixio Barrios <rbarrios@perscholas.org>
2022-06-10 10:40:39 -0400

    Update lesson

---
+++ Week 1/reactmovies/src/MovieInfo.js
@@ -0,0 +1,26 @@
+// 1. IMPORT REACT
+import React from "react";
+
+// 2. ADDITIONAL IMPORTS
+
+// 3. CREATE COMPONENT
+class MovieInfo extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>Title: {this.props.searchResult.Title}</h1>
+        <h2>Year: {this.props.searchResult.Year}</h2>
+        <img
+          src={this.props.searchResult.Poster}
+          alt={this.props.searchResult.Title}
+        />
+        <h3>Genre: {this.props.searchResult.Genre}</h3>
+        <h4>Plot: {this.props.searchResult.Plot}</h4>
+      </div>
+    );
+  }
+}
+
+// 4. EXPORT COMPONENT
+export default MovieInfo;
